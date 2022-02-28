7e202cf52459e2311b4500259790e2bd9c55b7bb
ejkimDev <ejkim.dev@gmail.com>
2022-03-01 02:49:44 +0900

    app.vue update

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@ export default {
   /*
   * 라이프사이클 (작성 순서는 상관X)
   * beforeCreate : 컴포넌트가 생성되기 직전에 실행, 활용도가 적다.
-  * created : App.vue라는 컴포넌트를 초기화하는 과정에서 컴포넌트가 생성된 직후에 실행
+  * created : App.vue라는 컴포넌트를 초기화하는 과정에서 컴포넌트가 생성된 직후에 실행, 활용도가 높다
   * beforeMount : 컴포넌트가 HTML 구조에 연결되기 전
-  * mounted : 컴포넌트가 HTML 구조에 연결된 후. 화면에 컴포넌트가 출력된 후 실행
+  * mounted : 컴포넌트가 HTML 구조에 연결된 후. 화면에 컴포넌트가 출력된 후 실행, 활용도가 높다
    */
   beforeCreate() {
     console.log('beforeCreate', this.count);
